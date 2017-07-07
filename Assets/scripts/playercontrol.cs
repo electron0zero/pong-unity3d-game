ce5ffadc1601677fa2eef381a7dd330132bb2038
Suraj Nath <devversitymail@gmail.com>
2017-07-08 00:49:10 +0530

    re-arrange project structure
    Now I know how to use Git, so fixing this.
    Hope it helps someone :-)

---
+++ Assets/scripts/playercontrol.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playercontrol : MonoBehaviour	 {
+
+	void FixedUpdate()	{
+
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.AddForce (Vector3);
+
+
+	}
+
+
+}
